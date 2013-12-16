e7cdab1fdea4164e9cf0222d4c5e402b68baea67
DariusL <d.lapunas@gmail.com>
2013-12-16 22:45:46 +0200

    doing stuff

--- Algoritmai-22/main.cpp
+++ Algoritmai-22/main.cpp
@@ -2,12 +2,16 @@
 #include <numeric>
 #include <ostream>
 #include <string>
+#include <random>
+#include <functional>
+#include <iostream>
 using namespace std;
 
 typedef size_t uint;
 
-vector<uint> ComputeBreaks(const vector<string> &words, uint n, uint m);
+vector<uint> ComputeBreaks(const vector<string> &words, uint m);
 void Output(const vector<string> &words, ostream &stream, vector<uint> p, uint j);
+vector<string> RandomData(uint diffWords, uint wordCount, uint maxLength);
 
 template<uint N>
 inline uint Pow(uint a)
@@ -23,14 +27,48 @@ inline uint Pow<0>(uint a)
 
 int main()
 {
+	auto &stream = cout;
+	auto words = RandomData(3, 10, 7);
+	auto breaks = ComputeBreaks(words, 10);
+	for (auto &word : words)
+		stream << word << ' ';
+	stream << endl << endl;
+	Output(words, stream, breaks, words.size() - 1);
+	system("pause");
 	return 0;
 }
 
-vector<uint> ComputeBreaks(const vector<string> &words, uint n, uint m)
+vector<string> RandomData(uint diffWords, uint wordCount, uint maxLength)
+{
+	vector<string> things;
+	default_random_engine gen;
+	uniform_int_distribution<uint> charDist(97, 122);
+	uniform_int_distribution<uint> lengthDist(0, maxLength);
+	uniform_int_distribution<uint> shuffleDist(0, diffWords - 1);
+	string temp;
+	for (uint i = 0; i < diffWords; i++)
+	{
+		uint l = lengthDist(gen);
+		for (uint j = 0; j < l; j++)
+		{
+			temp += charDist(gen);
+		}
+		things.push_back(move(temp));
+	}
+	vector<string> ret;
+	for (uint i = 0; i < wordCount; i++)
+	{
+		ret.push_back(things[shuffleDist(gen)]);
+	}
+	return ret;
+}
+
+vector<uint> ComputeBreaks(const vector<string> &words, uint m)
 {
 	vector<vector<uint>> extras, lc;
 	vector<uint> p, c;
 	const uint max = numeric_limits<uint>::max();
+	uint n = words.size();
 	for (uint i = 0; i < n; i++)
 	{
 		extras.emplace_back();
@@ -77,9 +115,9 @@ vector<uint> ComputeBreaks(const vector<string> &words, uint n, uint m)
 		p.emplace_back();
 		for (uint i = 0; i < j; i++)
 		{
-			if (c[i - 1 + lc[i][j]] < c[j])
+			if (c[i] + lc[i][j] < c[j+1])
 			{
-				c[j] = c[i - 1] + lc[i][j];
+				c[j+1] = c[i] + lc[i][j];
 				p[j] = i;
 			}
 		}
@@ -93,6 +131,6 @@ void Output(const vector<string> &words, ostream &stream, vector<uint> p, uint j
 	if (i != 0)
 		Output(words, stream, p, i - 1);
 	for (uint l = i; l <= j; l++)
-		stream << words[l];
+		stream << words[l] << ' ';
 	stream << endl;
 }
