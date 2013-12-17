f9f21cea3751184a3e574007dd989218692a4f5b
DariusL <d.lapunas@gmail.com>
2013-12-17 23:09:19 +0200

    testing

--- Algoritmai-22/main.cpp
+++ Algoritmai-22/main.cpp
@@ -5,9 +5,12 @@
 #include <random>
 #include <functional>
 #include <iostream>
+#include <fstream>
+#include <ctime>
 using namespace std;
 
 typedef size_t uint;
+uint ops = 0;
 
 vector<uint> ComputeBreaks(const vector<string> &words, uint m);
 void Output(const vector<string> &words, ostream &stream, vector<uint> p, uint j);
@@ -27,13 +30,19 @@ inline uint Pow<0>(uint a)
 
 int main()
 {
-	auto &stream = cout;
-	auto words = RandomData(10, 50, 7);
-	auto breaks = ComputeBreaks(words, 15);
-	for (auto &word : words)
-		stream << word << ' ';
-	stream << endl << endl;
-	Output(words, stream, breaks, words.size() - 1);
+	const uint counts[] = { 100, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000 };
+	ofstream out("rez.csv", ios::trunc);
+	uint time;
+	for (auto count : counts)
+	{
+		ops = 0;
+		auto words = RandomData(100, count, 10);
+		time = clock();
+		auto breaks = ComputeBreaks(words, 20);
+		time = clock() - time;
+		cout << " count " << count << " time " << time << " ops " << ops << endl;
+		out << count << ';' << time << ';' << ops << ";\n";
+	}
 	system("pause");
 	return 0;
 }
@@ -65,45 +74,45 @@ vector<string> RandomData(uint diffWords, uint wordCount, uint maxLength)
 
 vector<uint> ComputeBreaks(const vector<string> &words, uint m)
 {
-	vector<vector<int>> extras;
-	vector<vector<uint>> lc;
-	vector<uint> p, c;
 	const int max = numeric_limits<int>::max();
 	const uint n = words.size();
+	vector<uint> p, c;
+	vector<vector<int>> extras(n, vector<int>(n));
+	vector<vector<uint>> lc(n, vector<uint>(n));
+
+	ops += 7;
+
 	for (uint i = 0; i < n; i++)
 	{
-		extras.emplace_back();
-		lc.emplace_back();
-		for (uint j = 0; j < n; j++)
-		{
-			extras[i].emplace_back();
-			lc[i].emplace_back();
-		}
-	}
-	for (uint i = 0; i < n; i++)
-	{
+		ops += 3;
 		extras[i][i] = m - words[i].length();
 		for (uint j = i + 1; j < n; j++)
 		{
+			ops += 4;
 			extras[i][j] = extras[i][j - 1] - words[j].length() - 1;
 		}
 	}
 	for (uint i = 0; i < n; i++)
 	{
+		ops += 2;
 		for (uint j = i; j < n; j++)
 		{
+			ops += 2;
 			if (extras[i][j] < 0)
 			{
+				ops += 2;
 				lc[i][j] = max;
 			}
 			else
 			{
 				if (j == n - 1 && extras[i][j] >= 0)
 				{
+					ops += 2;
 					lc[i][j] = 0;
 				}
 				else
 				{
+					ops += 2;
 					lc[i][j] = Pow<3>(extras[i][j]);
 				}
 			}
@@ -112,12 +121,15 @@ vector<uint> ComputeBreaks(const vector<string> &words, uint m)
 	c.push_back(0);
 	for (uint j = 0; j < n; j++)
 	{
+		ops += 4;
 		c.push_back(max);
 		p.emplace_back();
 		for (uint i = 0; i <= j; i++)
 		{
+			ops += 2;
 			if (c[i] + lc[i][j] < c[j+1])
 			{
+				ops += 3;
 				c[j+1] = c[i] + lc[i][j];
 				p[j] = i;
 			}
